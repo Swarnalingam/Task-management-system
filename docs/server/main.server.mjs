f11b227b61b7083c9b5dd56c8ac0c27c464427be
Swarnalingam <kslinga06@gmail.com>
2025-07-24 12:57:54 +0530

    build application

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-Q2ZWB3JO.mjs";import"./chunk-U5EYRUSF.mjs";import"./chunk-BHGXM7IC.mjs";import"./chunk-2BIEJCSD.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
